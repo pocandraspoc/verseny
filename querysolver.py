d1e83ed6bbd1e2c743eaffa0a26e7f24ccde2b1c
sandris <andras.sereny@gmail.com>
2018-06-08 19:21:48 +0200

    Wire in Roman conversion to querysolver

--- querysolver.py
+++ querysolver.py
@@ -1,14 +1,40 @@
 import webhandler
 from rpnevaluator import RpnEvaluator
-from infix_to_rpn import infix_to_postfix
+from infix_to_rpn import infix_to_postfix, OPERATORS, PARENTHESIS
+import roman
+
 
 class QuerySolver(object):
     def __init__(self):
         pass
 
+    def has_digit(self, query):
+        for char in query:
+            if char.isdigit():
+                return True
+        return False
+
+    def to_arabic(self, roman_postfix):
+        result = []
+        for token in roman_postfix:
+            if token in OPERATORS.keys() or token in PARENTHESIS:
+                result.append(token)
+            else:
+                result.append(str(roman.convert_to_int(token)))
+        return result
+
+
     def answer_query(self, query):
         """Answer a query"""
         print(query)
-        tokens = query.split(' ')
-        postfix = infix_to_postfix(tokens)
-        return RpnEvaluator().evaluate_rpn(' '.join(postfix))
+        if self.has_digit(query):
+            tokens = query.split(' ')
+            postfix = infix_to_postfix(tokens)
+            return RpnEvaluator().evaluate_rpn(' '.join(postfix))
+        else:
+            tokens = query.split(' ')
+            postfix = infix_to_postfix(tokens)
+            arabic = self.to_arabic(postfix)
+            int_result = RpnEvaluator().evaluate_rpn(' '.join(arabic))
+            return roman.int_to_roman_numberal(int_result)
+
