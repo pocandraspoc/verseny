511201176cab8a3f2d90af4a05aa48224f9e3b08
Palotás Péter <peter.palotas@evosoft.com>
2018-06-08 19:35:17 +0200

    Leap year mock

--- querysolver.py
+++ querysolver.py
@@ -2,6 +2,7 @@ import webhandler
 from rpnevaluator import RpnEvaluator
 from infix_to_rpn import infix_to_postfix, OPERATORS, PARENTHESIS
 import roman
+import re
 
 
 class QuerySolver(object):
@@ -27,14 +28,20 @@ class QuerySolver(object):
     def answer_query(self, query):
         """Answer a query"""
         print(query)
-        if self.has_digit(query):
-            tokens = query.split(' ')
-            postfix = infix_to_postfix(tokens)
-            return RpnEvaluator().evaluate_rpn(' '.join(postfix))
+        if re.match(".*leap.*", query):
+            return "No"
         else:
-            tokens = query.split(' ')
-            postfix = infix_to_postfix(tokens)
-            arabic = self.to_arabic(postfix)
-            int_result = RpnEvaluator().evaluate_rpn(' '.join(arabic))
-            return roman.int_to_roman_numberal(int_result)
+            try:
+                if self.has_digit(query):
+                    tokens = query.split(' ')
+                    postfix = infix_to_postfix(tokens)
+                    return RpnEvaluator().evaluate_rpn(' '.join(postfix))
+                else:
+                    tokens = query.split(' ')
+                    postfix = infix_to_postfix(tokens)
+                    arabic = self.to_arabic(postfix)
+                    int_result = RpnEvaluator().evaluate_rpn(' '.join(arabic))
+                    return roman.int_to_roman_numberal(int_result)
+            except:
+                pass
 
